b8694f1be69f5ede778a4a1c0332724f8183f123
datlt4 <luongtandat.m0145@gmail.com>
2024-04-20 10:16:36 +0700

    build on jetson nano docker

--- python/setup.py
+++ python/setup.py
@@ -2,8 +2,9 @@ from setuptools import setup
 from setuptools_rust import Binding, RustExtension
 
 setup(
+    version='1.0.0',  # Specify the version here
     rust_extensions=[RustExtension('sqlite_zstd.libsqlite_zstd',
-        path='../Cargo.toml',
+        path='/app/sqlite-zstd/Cargo.toml',
         binding=Binding.NoBinding,
         features=['build_extension'],
         py_limited_api=True,
